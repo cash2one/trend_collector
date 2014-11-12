dc0bf1d49d5491f5f5e9d453ce17a2ba35416049
mosaiclife <mosaiclife@naver.com>
2014-11-12 14:18:49 +0900

    no message

---
+++ HTMLParseTest.py
@@ -0,0 +1,14 @@
+#-*- coding: utf-8 -*-
+import urllib
+import sys
+from bs4 import BeautifulSoup
+
+#reload(sys)
+#sys.setdefaultencoding('utf-8')
+
+#html = urllib.urlopen('http://m.search.naver.com/search.naver?query=')
+#soup = BeautifulSoup(html, 'lxml')
+
+#print(soup.prettify().encode('utf-8'))
+
+print '홍홍  aa'
